bfd397039d3443cc0d0943b6eda9215dc06a78b0
Jonathan Lim <jonathanlimes@gmail.com>
2017-01-24 18:50:21 +0800

    granularity almost there! hours mins secs influence each other but not accurate

--- clock.js
+++ clock.js
@@ -8,63 +8,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // BONUS SECTION
   var now = new Date()
-  var time = now.toLocaleTimeString()
-  var timeArr = time.split(':')
-  var currentHour = parseInt(timeArr[0])
-  function amPMHour () {
-    if (currentHour > 12) {
-      return currentHour - 12
-    } else {
-      return currentHour
-    }
-  }
-  var formattedHour = amPMHour()
-  var currentMinute = parseInt(timeArr[1])
-  var currentSecond = parseInt(timeArr[2])
+  var currentHour = now.getHours() % 12
+  var currentMinute = now.getMinutes()
+  var currentSecond = now.getSeconds()
 
-  function rotation (num) {
-    if (num === 60) {
+  function secondRotation (secs) {
+    if (secs === 60) {
       return 0
     } else {
-      return (num / 60) * 360
+      return secs * (360 / 60)
     }
   }
 
-  function hourRotation (num) {
-    if (num === 12) {
+  function minuteRotation (mins, secs) {
+    if (mins === 60) {
       return 0
     } else {
-      return (num / 12) * 360
+      return (mins * (360 / 60)) + (secs * (360 / 60 / 60))
+    }
+  }
+
+  function hourRotation (hrs, mins, secs) {
+    if (hrs === 12) {
+      return 0
+    } else {
+      return (hrs * (360 / 12)) + (mins * (360 / 12 / 60)) + (secs * (360 / 12 / 60 / 60))
     }
   }
 
   var secondCount = currentSecond
-  setInterval(tickSecond, 1000)
+  setInterval(tickSecond, 100)
   function tickSecond () {
     secondCount++
-    second.style.transform = 'rotate(' + rotation(secondCount) + 'deg)'
+    second.style.transform = 'rotate(' + secondRotation(secondCount) + 'deg)'
     if (secondCount === 60) {
       secondCount = 0
     }
   }
 
   var minuteCount = currentMinute
-  minute.style.transform = 'rotate(' + rotation(minuteCount) + 'deg)'
-  setInterval(tickMinute, 1000 * 60)
+  minute.style.transform = 'rotate(' + minuteRotation(minuteCount, secondCount) + 'deg)'
+  setInterval(tickMinute, 100 * 60)
   function tickMinute () {
     minuteCount++
-    minute.style.transform = 'rotate(' + rotation(minuteCount) + 'deg)'
+    minute.style.transform = 'rotate(' + minuteRotation(minuteCount, secondCount) + 'deg)'
     if (minuteCount === 60) {
       minuteCount = 0
     }
   }
 
-  var hourCount = formattedHour
-  hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
-  setInterval(tickHour, 1000 * 60 * 60)
+  var hourCount = currentHour
+  hour.style.transform = 'rotate(' + hourRotation(hourCount, minuteCount, secondCount) + 'deg)'
+  setInterval(tickHour, 100 * 60 * 60)
   function tickHour () {
     hourCount++
-    hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
+    hour.style.transform = 'rotate(' + hourRotation(hourCount, minuteCount, secondCount) + 'deg)'
     if (hourCount === 12) {
       hourCount = 0
     }
