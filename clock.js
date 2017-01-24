661b2c5fe726a90e2f85b98e4be18fa6723e23e9
Jonathan Lim <jonathanlimes@gmail.com>
2017-01-24 17:45:58 +0800

    developed vars to extract current time

--- clock.js
+++ clock.js
@@ -6,48 +6,40 @@ document.addEventListener('DOMContentLoaded', function () {
   clock.appendChild(minute)
   clock.appendChild(second)
 
-  function tick (hand) {
-    if (hand === second || hand === minute) {
-      hand.style.transform = 'rotate(' + secondMinRotation(1) + 'deg)'
-    } else if (hand === hour) {
-      hand.style.transform = 'rotate(' + degrees + 'deg)'
-    }
-  }
-
   // set counter,
   var secondCount = 0
   // every 1000ms,
-  setInterval(tickSecond, 1000)
+  setInterval(tickSecond, 10)
   // run tick function which will rotate the sec hand
   function tickSecond () {
     secondCount++
-    second.style.transform = 'rotate(' + secondMinRotation(secondCount) + 'deg)'
+    second.style.transform = 'rotate(' + rotation(secondCount) + 'deg)'
     if (secondCount === 60) {
       secondCount = 0
     }
   }
 
   var minuteCount = 0
-  setInterval(tickMinute, 1000 * 60)
+  setInterval(tickMinute, 10 * 60)
   function tickMinute () {
     minuteCount++
-    minute.style.transform = 'rotate(' + secondMinRotation(minuteCount) + 'deg)'
+    minute.style.transform = 'rotate(' + rotation(minuteCount) + 'deg)'
     if (minuteCount === 60) {
       minuteCount = 0
     }
   }
 
   var hourCount = 0
-  setInterval(tickHour, 1000 * 60 * 60)
+  setInterval(tickHour, 10 * 60 * 2)
   function tickHour () {
     hourCount++
     hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
-    if (hourCount === 12) {
+    if (hourCount === 360) {
       hourCount = 0
     }
   }
 
-  function secondMinRotation (num) {
+  function rotation (num) {
     if (num === 60) {
       return 0
     } else {
@@ -56,10 +48,18 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function hourRotation (num) {
-    if (num === 12) {
+    if (num === 360) {
       return 0
     } else {
-      return (num / 12) * 360
+      return num
     }
   }
+
+  //BONUS
+  var now = new Date()
+  var time = now.toLocaleTimeString()
+  var timeArr = time.split(":")
+  var currentHour = timeArr[0]
+  var currentMinute = timeArr[1]
+  var currentSecond = timeArr[2]
 })
