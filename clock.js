46342c75aa9ef43c4fce4d99d59879644112e2ca
Jonathan Lim <jonathanlimes@gmail.com>
2017-01-24 17:58:44 +0800

    second hand now reflects real time but the rest are broken

--- clock.js
+++ clock.js
@@ -6,38 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
   clock.appendChild(minute)
   clock.appendChild(second)
 
-  // set counter,
-  var secondCount = 0
-  // every 1000ms,
-  setInterval(tickSecond, 10)
-  // run tick function which will rotate the sec hand
-  function tickSecond () {
-    secondCount++
-    second.style.transform = 'rotate(' + rotation(secondCount) + 'deg)'
-    if (secondCount === 60) {
-      secondCount = 0
-    }
-  }
-
-  var minuteCount = 0
-  setInterval(tickMinute, 10 * 60)
-  function tickMinute () {
-    minuteCount++
-    minute.style.transform = 'rotate(' + rotation(minuteCount) + 'deg)'
-    if (minuteCount === 60) {
-      minuteCount = 0
-    }
-  }
-
-  var hourCount = 0
-  setInterval(tickHour, 10 * 60 * 2)
-  function tickHour () {
-    hourCount++
-    hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
-    if (hourCount === 360) {
-      hourCount = 0
-    }
-  }
+  // BONUS SECTION
+  var now = new Date()
+  var time = now.toLocaleTimeString()
+  var timeArr = time.split(':')
+  var currentHour = parseInt(timeArr[0])
+  var currentMinute = parseInt(timeArr[1])
+  var currentSecond = parseInt(timeArr[2])
 
   function rotation (num) {
     if (num === 60) {
@@ -55,11 +30,33 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  //BONUS
-  var now = new Date()
-  var time = now.toLocaleTimeString()
-  var timeArr = time.split(":")
-  var currentHour = timeArr[0]
-  var currentMinute = timeArr[1]
-  var currentSecond = timeArr[2]
+  var secondCount = currentSecond
+  setInterval(tickSecond, 1000)
+  function tickSecond () {
+    secondCount++
+    second.style.transform = 'rotate(' + rotation(secondCount) + 'deg)'
+    if (secondCount === 60) {
+      secondCount = 0
+    }
+  }
+
+  var minuteCount = currentMinute
+  setInterval(tickMinute, 1000 * 60)
+  function tickMinute () {
+    minuteCount++
+    minute.style.transform = 'rotate(' + rotation(minuteCount) + 'deg)'
+    if (minuteCount === 60) {
+      minuteCount = 0
+    }
+  }
+
+  var hourCount = currentHour
+  setInterval(tickHour, 1000 * 60 * 2)
+  function tickHour () {
+    hourCount++
+    hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
+    if (hourCount === 360) {
+      hourCount = 0
+    }
+  }
 })
