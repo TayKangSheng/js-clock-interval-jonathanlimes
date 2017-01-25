bf8599311cddd982746a8809d173ee36824ab0c8
Jonathan Lim <jonathanlimes@gmail.com>
2017-01-25 10:21:34 +0800

    tidied up code and removed unnecessary if statements

--- clock.js
+++ clock.js
@@ -17,45 +17,48 @@ document.addEventListener('DOMContentLoaded', function () {
   function minuteRotation (mins) {
     return mins * (360 / 60)
   }
-  function hourRotation (hrs) {
-    return hrs * (360 / 12)
+  function hourRotation (hrs, mins, secs) {
+    return hrs * (360 / 12) + mins * (360 / 12 / 60)
   }
 
   var secondCount = currentSecond
+  second.style.transform = 'rotate(' + secondRotation(secondCount) + 'deg)'
   setInterval(tickSecond, 1000)
   function tickSecond () {
     secondCount++
     second.style.transform = 'rotate(' + secondRotation(secondCount) + 'deg)'
-    second.style.transition = 'transform 0.2s cubic-bezier(.4,2.08,.55,.44)'
-    if (secondCount === 60) {
-      secondCount = 0
-    }
+    second.style.transition = 'transform 0.2s cubic-bezier(.5,2.08,.55,.44)'
+    // if (secondCount === 60) {
+    //   secondCount = 0
+    // }
   }
 
   var minuteCount = currentMinute
   minute.style.transform = 'rotate(' + minuteRotation(minuteCount) + 'deg)'
+  console.log(currentMinute)
   if (secondCount !== 0) {
     var secondDelay = 60 - secondCount
     setTimeout(setInterval(tickMinute, 1000 * 60), 1000 * secondDelay)
   }
+  setInterval(tickMinute, 1000 * 60)
   function tickMinute () {
     minuteCount++
     minute.style.transform = 'rotate(' + minuteRotation(minuteCount) + 'deg)'
     minute.style.transition = 'transform 0.3s cubic-bezier(.4,2.08,.55,.44)'
-    if (minuteCount === 60) {
-      minuteCount = 0
-    }
+    // if (minuteCount === 60) {
+    //   minuteCount = 0
+    // }
   }
 
   var hourCount = currentHour
-  hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
+  hour.style.transform = 'rotate(' + hourRotation(hourCount, minuteCount, secondCount) + 'deg)'
   if (minuteCount !== 60) {
     var minuteDelay = 60 - minuteCount
     setTimeout(setInterval(tickHour, 1000 * 60 * 60), 1000 * 60 * minuteDelay)
   }
   function tickHour () {
     hourCount++
-    hour.style.transform = 'rotate(' + hourRotation(hourCount) + 'deg)'
+    hour.style.transform = 'rotate(' + hourRotation(hourCount, minuteCount, secondCount) + 'deg)'
     if (hourCount === 12) {
       hourCount = 0
     }
